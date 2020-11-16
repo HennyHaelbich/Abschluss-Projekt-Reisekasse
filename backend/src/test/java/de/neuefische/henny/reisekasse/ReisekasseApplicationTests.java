17b3062493cfa4c59f5b0c4ba11ad384efcf63a0
Henny Haelbich <henny.haelbich@gmail.com>
2020-11-16 12:03:37 +0100

    Add backend

---
+++ backend/src/test/java/de/neuefische/henny/reisekasse/ReisekasseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.henny.reisekasse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReisekasseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
