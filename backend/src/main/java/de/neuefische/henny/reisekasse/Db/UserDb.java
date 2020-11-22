b3bf31004ee208b515e3a4cd90203c7b7c9dcc5f
Henny Haelbich <henny.haelbich@gmail.com>
2020-11-22 16:40:38 +0100

    delete userDb.findById

--- backend/src/main/java/de/neuefische/henny/reisekasse/Db/UserDb.java
+++ backend/src/main/java/de/neuefische/henny/reisekasse/Db/UserDb.java
@@ -3,8 +3,5 @@ package de.neuefische.henny.reisekasse.Db;
 import de.neuefische.henny.reisekasse.Model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.Optional;
-
 public interface UserDb extends PagingAndSortingRepository<User, String> {
-    Optional<User> findById(String username);
 }
