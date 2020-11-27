4846570a288aa9d80a54d99cf4a0dd6dda7f81ce
Henny Haelbich <henny.haelbich@gmail.com>
2020-11-27 13:30:06 +0100

    resolve error

--- backend/src/main/java/de/neuefische/henny/reisekasse/db/UserDb.java
+++ backend/src/main/java/de/neuefische/henny/reisekasse/db/UserDb.java
@@ -1,7 +1,7 @@
 package de.neuefische.henny.reisekasse.db;
 
-import de.neuefische.henny.reisekasse.model.User;
+import de.neuefische.henny.reisekasse.model.TravelFoundUser;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserDb extends PagingAndSortingRepository<User, String> {
+public interface UserDb extends PagingAndSortingRepository<TravelFoundUser, String> {
 }
