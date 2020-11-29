04e032b690b8e0acbbb5a943e6f9f9c4aa763f45
Henny Haelbich <henny.haelbich@gmail.com>
2020-11-29 09:29:22 +0100

    delete console log in SignUpService

--- frontend/src/service/SignUpService.js
+++ frontend/src/service/SignUpService.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const signUp = (signUpData) => {
   const { username, password, firstName, lastName } = signUpData
-  console.log("SignUpService", { username, password, firstName, lastName })
   return axios
     .post('/auth/registration', { username, password, firstName, lastName })
     .then((response) => response.data)
