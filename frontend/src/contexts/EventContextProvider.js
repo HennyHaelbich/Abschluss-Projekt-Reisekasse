b6daf99fe527b21dff9b8bf7a61493856cb8a18e
Henny Haelbich <henny.haelbich@gmail.com>
2020-12-02 11:33:04 +0100

    change typing error

--- frontend/src/contexts/EventContextProvider.js
+++ frontend/src/contexts/EventContextProvider.js
@@ -25,7 +25,7 @@ export default function EventContextProvider({ children }) {
     axios
       .post('/api/events/' + id, {description, members, payer, amount}, header(token))
       .then((response) => response.data)
-      .then((updatedEvent) => setEvents(events.map((event) => event.id === id ? updateEvent : event)))
+      .then((updatedEvent) => setEvents(events.map((event) => event.id === id ? updatedEvent : event)))
       .catch(console.log);
   
 
@@ -40,7 +40,7 @@ export default function EventContextProvider({ children }) {
     axios
       .put('/api/events/expenditure/delete', {eventId, expenditureId}, header(token))
       .then((response) => response.data)
-      .then((updatedEvent) => setEvents(events.map((event) => event.id === eventId ? updateEvent : event)))
+      .then((updatedEvent) => setEvents(events.map((event) => event.id === eventId ? updatedEvent : event)))
       .catch(console.log);
     
   return (
