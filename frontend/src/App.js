6487e84914cd6417cac1af4f257ba03ec3064f20
Henny Haelbich <henny.haelbich@gmail.com>
2020-12-05 16:09:22 +0100

    add MaterialUi thene to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import AddEventForm from './addEventPage/AddEventForm';
 import EventContextProvider from './contexts/EventContextProvider';
 import ShowEvent from './eventPage/ShowEvent';
@@ -12,39 +12,56 @@ import AddExpenditureForm from './addExpenditurePage/AddExpenditureForm';
 import SignUpForm from './signUpPage/SignUpForm';
 import ExpenditurePage from './expenditurePage/ExpenditurePage';
 import CompensationPage from './compensationPage/CompensationPage';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#5a189a',
+    },
+    secondary: {
+      main: '#ff8500',
+    },
+  },
+});
 
 function App() {
   return (
     <LoginContextProvider>
       <EventContextProvider>
-        <PageLayout>
-          <Switch>
-            <Route path={'/signup'} component={SignUpForm} />
-            <Route path={'/login'} component={LoginPage} />
-            <ProtectedRoute path={'/new'} component={AddEventForm} />
-            <ProtectedRoute path={'/events'} component={EventsPage} />
-            <ProtectedRoute
-              exact
-              path={'/event/compensation/:eventId'}
-              component={CompensationPage}
-            />
-            <ProtectedRoute
-              exact
-              path={'/event/new-expand/:eventId'}
-              component={AddExpenditureForm}
-            />
-            <ProtectedRoute
-              exact
-              path={'/event/:eventId/:expenditureId'}
-              component={ExpenditurePage}
-            />
-            <ProtectedRoute
-              exact
-              path={'/event/:eventId'}
-              component={ShowEvent}
-            />
-          </Switch>
-        </PageLayout>
+        <MuiThemeProvider theme={theme}>
+          <PageLayout>
+            <Switch>
+              <Route path={'/signup'} component={SignUpForm} />
+              <Route path={'/login'} component={LoginPage} />
+              <ProtectedRoute path={'/new'} component={AddEventForm} />
+              <ProtectedRoute path={'/events'} component={EventsPage} />
+              <ProtectedRoute
+                exact
+                path={'/event/compensation/:eventId'}
+                component={CompensationPage}
+              />
+              <ProtectedRoute
+                exact
+                path={'/event/new-expand/:eventId'}
+                component={AddExpenditureForm}
+              />
+              <ProtectedRoute
+                exact
+                path={'/event/:eventId/:expenditureId'}
+                component={ExpenditurePage}
+              />
+              <ProtectedRoute
+                exact
+                path={'/event/:eventId'}
+                component={ShowEvent}
+              />
+              <Route path="/">
+                <Redirect to="/events" />
+              </Route>
+            </Switch>
+          </PageLayout>
+        </MuiThemeProvider>
       </EventContextProvider>
     </LoginContextProvider>
   );
--- frontend/src/styling/AddButton.js
+++ frontend/src/styling/AddButton.js
@@ -2,24 +2,13 @@ import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import React from 'react';
 import styled from 'styled-components/macro';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#5a189a',
-    },
-  },
-});
 
 export default function AddButton({ handle }) {
   return (
     <DivStyled>
-      <MuiThemeProvider theme={theme}>
-        <Fab color="primary" aria-label="add" onClick={() => handle()}>
-          <AddIcon />
-        </Fab>
-      </MuiThemeProvider>
+      <Fab color="primary" aria-label="add" onClick={() => handle()}>
+        <AddIcon />
+      </Fab>
     </DivStyled>
   );
 }
