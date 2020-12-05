cb6b6a542e1b4bbdfcbed95f8e545f1af2411e1f
Henny Haelbich <henny.haelbich@gmail.com>
2020-12-05 08:51:08 +0100

    use prettier in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import AddEventForm from './addEventPage/AddEventForm';
 import EventContextProvider from './contexts/EventContextProvider';
 import ShowEvent from './eventPage/ShowEvent';
 import EventsPage from './eventsListPage/EventsPage';
-import LoginPage from "./loginPage/LoginPage";
-import LoginContextProvider from "./contexts/LoginContextProvider";
-import ProtectedRoute from "./routing/ProtectedRoute";
+import LoginPage from './loginPage/LoginPage';
+import LoginContextProvider from './contexts/LoginContextProvider';
+import ProtectedRoute from './routing/ProtectedRoute';
 import AddExpenditureForm from './addExpenditurePage/AddExpenditureForm';
-import SignUpForm from "./signUpPage/SignUpForm";
-import ExpenditurePage from "./expenditurePage/ExpenditurePage";
-import CompensationPage from "./compensationPage/CompensationPage";
+import SignUpForm from './signUpPage/SignUpForm';
+import ExpenditurePage from './expenditurePage/ExpenditurePage';
+import CompensationPage from './compensationPage/CompensationPage';
 
 function App() {
   return (
@@ -21,10 +21,26 @@ function App() {
           <Route path={'/login'} component={LoginPage} />
           <ProtectedRoute path={'/new'} component={AddEventForm} />
           <ProtectedRoute path={'/events'} component={EventsPage} />
-          <ProtectedRoute exact path={'/event/compensation/:eventId'} component={CompensationPage} />
-          <ProtectedRoute exact path={'/event/new-expand/:eventId'} component={AddExpenditureForm} />
-          <ProtectedRoute exact path={'/event/:eventId/:expenditureId'} component={ExpenditurePage} />
-          <ProtectedRoute exact path={'/event/:eventId'} component={ShowEvent} />
+          <ProtectedRoute
+            exact
+            path={'/event/compensation/:eventId'}
+            component={CompensationPage}
+          />
+          <ProtectedRoute
+            exact
+            path={'/event/new-expand/:eventId'}
+            component={AddExpenditureForm}
+          />
+          <ProtectedRoute
+            exact
+            path={'/event/:eventId/:expenditureId'}
+            component={ExpenditurePage}
+          />
+          <ProtectedRoute
+            exact
+            path={'/event/:eventId'}
+            component={ShowEvent}
+          />
         </Switch>
       </EventContextProvider>
     </LoginContextProvider>
